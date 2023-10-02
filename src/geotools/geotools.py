996e3cfd7c85d385a65383ca63e70864b78949f9
holdb103 <62113897+holdb103@users.noreply.github.com>
2023-10-02 09:22:34 -0400

    Update geotools.py

--- src/geotools/geotools.py
+++ src/geotools/geotools.py
@@ -114,28 +114,7 @@ class GeoEngine():
             raise ApiException(response.status_code)
             
             
-class TestGeotools:
-    ge = GeoEngine()
-    def test_elevation_Mount_Everest(self):
-        assert int(self.ge.get_elevation((27.9881, 86.9250))) == 8752
         
-    def test_elevation_Death_Valley(self):
-        assert int(self.ge.get_elevation((27.9881, 86.9250))) == 8752
-        
-    def test_elevation_Bad_Coordinates(self):
-        with pytest.raises(ValueError):
-            self.ge.get_elevation([-200, 200])
-            
-    def test_soil_type_Cambisols(self):
-        assert self.ge.get_fao_soil_type((32.95047, -87.393259)) == "Cambisols"
-        
-    def test_soil_type_Water(self):
-        assert self.ge.get_fao_soil_type((0, 0)) == "Water"
-        
-    def test_soil_type_Bad_Coordinates(self):
-        with pytest.raises(ValueError):
-            self.ge.get_fao_soil_type([-200, 200])
-            
             
 class ApiException(Exception):
     def __init__(self, status_code):
